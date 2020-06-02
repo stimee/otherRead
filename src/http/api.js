17a5ac77e8c6780b90d4b06d5ed9c442326d4ef4
zhangguangjie <1611921969@qq.com>
2020-06-02 21:58:19 +0800

    请求添加代理、请求统一管理

--- src/http/api.js
+++ src/http/api.js
@@ -1,4 +1,11 @@
-
 export function login(params) {
-  return this.$http.post('/api/login',params)
+  return this.$http.post('/api/login', params)
+}
+
+export function initPage(params) {
+  return this.$http.post('/api/initPage', params, { emulateJSON: true })
+}
+
+export function userInfo(params) {
+  return this.$http.post('/api/userInfo', params, { emulateJSON: true })
 }
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 const path = require('path')
 module.exports = {
   devServer: {
-    port: '80',
+    port: '8080',
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-      }
-    }
+      '/api': {
+        target: 'http://123.56.134.30',
+        pathRewrite: { '^/api': '' },
+      },
+    },
   },
   configureWebpack: {
-    
     resolve: {
       alias: {
         pages: path.resolve(__dirname, 'src/pages/'),
         assets: path.resolve(__dirname, 'src/assets/'),
         components: path.resolve(__dirname, 'src/components/'),
         http: path.resolve(__dirname, 'src/http/'),
-      }
-    }
-  }
+      },
+    },
+  },
 }
